6cd7376d86fa43c708f97b2e3a476645b5571e86
Administracio <administracio@iMacAdmin.local>
2015-07-29 08:31:25 +0200

    Añadiendo modulos de Odoo para campos requeridos

---
+++ website_sale_state_required/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'e-commerce required state or province',
+    'summary': 'State or province required in checkout form',
+    'version': '1.0',
+    'category': 'Website',
+    'author': "Laura Muñoz Isidro",
+    'website': 'http://www.domestika.org/es/arual8/portfolio',
+    'license': 'AGPL-3',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'website_order_view.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ website_sale_state_required/controllers/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from openerp.addons.website_sale.controllers.main import website_sale
+
+class WebsiteSale(website_sale):
+    def checkout_form_validate(self, data):
+        res = super(WebsiteSale, self).checkout_form_validate(data)
+        if not data.get('state_id'):
+            res['state_id'] = 'missing'
+        return res
